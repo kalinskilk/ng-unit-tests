f8e54e91a2a51e7a81fcddfb6f2f539701595d77
kalinski_lk <lukkas_megafox@hotmail.com>
2025-10-07 15:35:18 -0300

    fix: using todo service on app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { TodosService } from './_services/todos';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('ng-unit-tests');
+
+  constructor(private todoService: TodosService) {}
+
+  ngOnInit() {
+    forkJoin({
+      todoAll: this.todoService.getAll(),
+      todoById: this.todoService.getById(1),
+    }).subscribe((result) => {
+      console.log(result.todoAll);
+      console.log(result.todoById);
+    });
+  }
 }
